c5bdef5712f89a2586cf0e28943c589e7ed818f8
Orel Balilti <orelba93@gmail.com>
2018-12-14 12:13:40 +0200

    artillery implemented

--- run.sh
+++ run.sh
@@ -7,45 +7,45 @@ echo > "$resultFile"
 
 #echo hapi >> "$resultFile"
 #node hapiserver.js & sleep 5
-#ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> "$resultFile"
+#npm run test http://127.0.0.1:8000/ | grep "Requests per second:" >> "$resultFile"
 #pkill -f hapiserver
 #sleep 5
 
 echo node-raw >> "$resultFile"
 node rawnodeserver.js & sleep 5
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> "$resultFile"
+npm run test http://127.0.0.1:8000/ | grep "Requests per second:" >> "$resultFile"
 pkill -f rawnodeserver
 sleep 5
 
  echo restify >> "$resultFile"
  node restifyserver.js & sleep 5
- ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> "$resultFile"
+ npm run test http://127.0.0.1:8000/ | grep "Requests per second:" >> "$resultFile"
  pkill -f restifyserver
  sleep 5
 
 
  echo koa2 >> "$resultFile"
  node koa2server.js & sleep 5
- ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> "$resultFile"
+ npm run test http://127.0.0.1:8000/ | grep "Requests per second:" >> "$resultFile"
  pkill -f koa2server
  sleep 5
 
 echo express >> "$resultFile"
 node expressserver.js & sleep 5
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> "$resultFile"
+npm run test http://127.0.0.1:8000/ | grep "Requests per second:" >> "$resultFile"
 pkill -f expressserver
 sleep 5
 
 echo SugoiJS >> "$resultFile"
 cd sugoijs/server && npm run start & sleep 5
 cd ../
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:3000/index | grep "Requests per second:" >> "$resultFile"
+npm run test http://127.0.0.1:3000/index | grep "Requests per second:" >> "$resultFile"
 pkill -f server
 sleep 5
 
 echo total.js >> "$resultFile"
 node totalserver/total.js & sleep 5
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> "$resultFile"
+npm run test http://127.0.0.1:8000/ | grep "Requests per second:" >> "$resultFile"
 pkill -f total
 sleep 5
 
@@ -53,7 +53,7 @@ echo sails.js >> "$resultFile"
 cd ./test-project
 sails lift & sleep 5
 cd ../
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:1337/ | grep "Requests per second:" >> "$resultFile"
+npm run test http://127.0.0.1:1337/ | grep "Requests per second:" >> "$resultFile"
 pkill -f sails
 sleep 5
 
@@ -61,13 +61,13 @@ sleep 5
 #cd yardstick
 #npm start run & sleep 5
 #cd ../
-#ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:3333/ | grep "Requests per second:" >> "$resultFile"
+#npm run test http://127.0.0.1:3333/ | grep "Requests per second:" >> "$resultFile"
 #pkill -f server.js
 #sleep 5
 
 echo NestJS >> "$resultFile"
 cd nestjs && npm run build && npm run start:prod && sleep 5
 cd ../
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:3000/ | grep "Requests per second:" >> "$resultFile"
+npm run test http://127.0.0.1:3000/ | grep "Requests per second:" >> "$resultFile"
 pkill -f node
 sleep 5
